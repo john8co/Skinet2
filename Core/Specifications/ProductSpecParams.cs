f2c68e0ca81e40c22b37ea9aed85f4cd8b25a04d
John8co <cornet.jonathan@gmail.com>
2026-06-03 21:19:01 +0200

    End of Section 5

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,41 @@
+namespace Core.Specifications;
+
+public class ProductSpecParams
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 6;
+    private List<string> _brands = [];    
+    private List<string> _types = [];
+    private string? _search;
+    
+
+    public int PageIndex { get; set; } = 1;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+    public List<string> Brands
+    {
+        get => _brands;
+        set
+        {
+            _brands = value.SelectMany(x => x.Split(',', StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+    public List<string> Types
+    {
+        get => _types;
+        set
+        {
+            _types = value.SelectMany(x => x.Split(',', StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+
+    public string? Sort { get; set; }    
+    public string Search
+    {
+        get => _search ?? ""; 
+        set => _search = value.ToLower();
+    }
+}
